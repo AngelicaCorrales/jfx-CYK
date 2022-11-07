3410f15bc5ffb138858ebea1441a05b6a219c535
Keren López <kerenlopezcordoba@outlook.es>
2022-11-06 21:46:07 -0500

    Changes in mainPane.fxml and background.jpeg file was added

--- src/model/ControllerCYK.java
+++ src/model/ControllerCYK.java
@@ -1,5 +1,11 @@
 package model;
 
+import javafx.scene.control.TextField;
+
 public class ControllerCYK {
+
+	public ControllerCYK(TextField stringW, Integer value) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/ui/CYKgui.java
+++ src/ui/CYKgui.java
@@ -6,9 +6,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import model.ControllerCYK;
 
@@ -19,10 +21,10 @@ public class CYKgui {
 	private BorderPane mainPane;
 
 	@FXML
-	private TextField inputSymbols;
+	private TextField stringW;
 
 	@FXML
-	private Spinner<Integer> statesSpinner;
+	private Spinner<Integer> variablesSpinner;
 
 	public CYKgui(ControllerCYK c) {
 		control=c;
@@ -36,11 +38,26 @@ public class CYKgui {
 		mainPane.setCenter(menuPane);
 		mainPane.setStyle("-fx-background-image: url(/ui/background.jpeg)");
 		SpinnerValueFactory<Integer> valueFactory= new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 26, 1);
-		statesSpinner.setValueFactory(valueFactory);
+		variablesSpinner.setValueFactory(valueFactory);
 	}
 	
 	@FXML
-	void continueToScreen2(ActionEvent event) {
+	public void continueToScreen2(ActionEvent event) throws IOException {
+		if(!stringW.getText().equals("")) {
+			control = new ControllerCYK(stringW, variablesSpinner.getValue());
 
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/screen2.fxml"));
+			fxmlLoader.setController(this);
+			Parent menuPane = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.setCenter(menuPane);
+			mainPane.setStyle("-fx-background-image: url(/ui/background.jpeg)");
+		}else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText(null);
+			alert.setContentText("Debe completar cada uno de los campos indicados para continuar.");
+			alert.showAndWait();
+		}
 	}
 }
